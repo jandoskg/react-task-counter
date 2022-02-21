91afdabb4d7ee9e8b249c8f75df394df1752309f
Jandos Bektursunov <jandosbektursunov@MacBook-Air-Jandos.local>
2022-02-21 09:56:29 +0600

    first commit

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import "../../App.css"
 
 const HomePage = () => {
     return (
         <>
         <center>
-            <h1 className='my-5 '>Hi, My Dear Frined</h1>
-            <center><h3 className='text-warning'>It is my first react project. Please, help me with a react Context, with a hook useEffect and useReduce, i can`t understand this thinks. But i am realy want to use this hooks in my live. PLEASE!!! Help me!!! </h3></center>
+            <h1 className='my-5 '>Hi, My Dear Friend</h1>
+            <div className='Home-text'>
+            <h3 >It is my first react project. <br /> Please, help me with a react Context, with a hook useEffect and useReduce, i can`t understand this thinks. But i am realy want to use this hooks in my live. PLEASE!!! Help me!!! </h3></div>
         </center>
 
             
--- src/components/meals/Burger.jsx
+++ src/components/meals/Burger.jsx
@@ -10,6 +10,7 @@ const Burger = () => {
             src="https://macdonaldsmenu.ru/wp-content/uploads/2021/05/%D0%A2%D0%B5%D1%85%D0%B0%D1%81-%D0%91%D1%83%D1%80%D0%B3%D0%B5%D1%80-%D0%B2-%D0%9C%D0%B0%D0%BA%D0%B4%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%B4%D1%81.jpg"
             alt="burger"
           />
+          <div className='div-text'>
           <p className="text">
             Техас Бургер от McDonald’s имеет следующий состав: <br /> жареный бекон
             (нарезка); <br /> соус на основе растительных масел «Томаты-Гриль»;
@@ -18,6 +19,7 @@ const Burger = () => {
             тостере;<br /> плавленый сыр «Чеддер»;<br /> репчатый лук резаный
             восстановленный.
           </p>
+          </div>
         </div>
       </>
     );
